7924cb5e5cf09dbb3ac31883e283b4a06debb240
John <sandbox.duchuy02@gmail.com>
2025-03-03 22:33:24 +0700

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,27 @@
+<div class="sidebar-wrapper scrollbar scrollbar-inner">
+    <div class="sidebar-content">
+        <ul class="nav nav-secondary">
+            <li class="nav-item">
+                <a
+                    data-bs-toggle="collapse"
+                    href="#dashboard"
+                    class="collapsed"
+                    aria-expanded="false"
+                >
+                    <i class="fas fa-home"></i>
+                    <p>Báo cáo thống kê</p>
+                    <span class="caret"></span>
+                </a>
+                <div class="collapse" id="dashboard">
+                    <ul class="nav nav-collapse">
+                        <li>
+                            <a href="#">
+                                <span class="sub-item">Báo cáo doanh số</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
