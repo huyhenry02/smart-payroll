23fa49b29811b01d040a222bcc74c44d508d171a
John <sandbox.duchuy02@gmail.com>
2025-03-29 23:43:40 +0700

    define header, sidebar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -31,12 +31,41 @@
         style="background-color: #a01f23"
     >
         <div class="container-fluid">
+            @php
+                use Illuminate\Support\Facades\Route;
+                $routeName = Route::currentRouteName();
+            @endphp
             <nav
                 class="navbar navbar-header-left navbar-expand-lg navbar-form nav-search p-0 d-none d-lg-flex"
                 style="color: #fff"
             >
                 <h3 style="color: #fff">
-                    Danh sách người dùng
+                    @switch( $routeName )
+                        @case('general_catalog.showIndexDepartment')
+                            Danh sách phòng ban
+                            @break
+                        @case('general_catalog.showIndexPosition')
+                            Danh sách chức vụ
+                            @break
+                        @case('general_catalog.showIndexEmployee')
+                            Danh sách nhân viên
+                            @break
+                        @case('general_catalog.showUpdateEmployee')
+                            Cập nhật nhân viên
+                            @break
+                        @case('general_catalog.showIndexWorkingShift')
+                            Danh sách ca làm việc
+                            @break
+                        @case('general_catalog.showIndexDeduction')
+                            Danh sách khấu trừ
+                            @break
+                        @case('general_catalog.showIndexAllowance')
+                            Danh sách phụ cấp
+                            @break
+                        @case('system.showIndexUser')
+                            Danh sách người dùng
+                            @break
+                    @endswitch
                 </h3>
                 <ul class="breadcrumbs">
                     <li class="nav-home">
@@ -48,13 +77,40 @@
                         <i class="icon-arrow-right"></i>
                     </li>
                     <li class="nav-item" style="color: #fff">
-                        Quản trị hệ thống
+                        @if ( Str::startsWith($routeName, 'general_catalog'))
+                            <a href="{{ route('general_catalog.showIndexEmployee') }}" style="color: #fff">Danh mục chung</a>
+                        @elseif ( Str::startsWith($routeName, 'system'))
+                            <a href="{{ route('system.showIndexUser') }}" style="color: #fff">Quản trị hệ thống</a>
+                        @endif
                     </li>
                     <li class="separator">
                         <i class="icon-arrow-right"></i>
                     </li>
                     <li class="nav-item">
-                        Người dùng
+                        @switch( $routeName )
+                            @case('general_catalog.showIndexDepartment')
+                                <a href="{{ route('general_catalog.showIndexDepartment') }}" style="color: #fff">Phòng ban</a>
+                                @break
+                            @case('general_catalog.showIndexPosition')
+                                <a href="{{ route('general_catalog.showIndexPosition') }}" style="color: #fff">Chức vụ</a>
+                                @break
+                            @case('general_catalog.showIndexEmployee')
+                            @case('general_catalog.showUpdateEmployee')
+                                <a href="{{ route('general_catalog.showIndexEmployee') }}" style="color: #fff">Nhân viên</a>
+                                @break
+                            @case('general_catalog.showIndexWorkingShift')
+                                <a href="{{ route('general_catalog.showIndexWorkingShift') }}" style="color: #fff">Ca làm thêm</a>
+                                @break
+                            @case('general_catalog.showIndexDeduction')
+                                <a href="{{ route('general_catalog.showIndexDeduction') }}" style="color: #fff">Trích nộp theo lương</a>
+                                @break
+                            @case('general_catalog.showIndexAllowance')
+                                <a href="{{ route('general_catalog.showIndexAllowance') }}" style="color: #fff">Phụ cấp và trợ cấp</a>
+                                @break
+                            @case('system.showIndexUser')
+                                <a href="{{ route('system.showIndexUser') }}" style="color: #fff">Người dùng</a>
+                                @break
+                        @endswitch
                     </li>
                 </ul>
             </nav>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,3 +1,25 @@
+<?php
+$routesSystem = [
+    'system.showIndexUser',
+];
+$isActiveSystem = collect($routesSystem)->contains(fn($route) => request()->routeIs($route));
+
+$routesGeneralCatalog = [
+    'general_catalog.showIndexDepartment',
+    'general_catalog.showIndexPosition',
+    'general_catalog.showIndexEmployee',
+    'general_catalog.showIndexWorkingShift',
+    'general_catalog.showIndexDeduction',
+    'general_catalog.showIndexAllowance',
+];
+$isActiveGeneralCatalog = collect($routesGeneralCatalog)->contains(fn($route) => request()->routeIs($route));
+
+$routesDepartmentPosition = [
+    'general_catalog.showIndexDepartment',
+    'general_catalog.showIndexPosition',
+];
+$isActiveDepartmentPosition = collect($routesDepartmentPosition)->contains(fn($route) => request()->routeIs($route));
+?>
 <div class="sidebar">
     <div class="sidebar-logo">
         <!-- Logo Header -->
@@ -28,20 +50,23 @@
     <div class="sidebar-wrapper scrollbar scrollbar-inner">
         <div class="sidebar-content">
             <ul class="nav nav-secondary">
-                <li class="nav-item">
+                <li class="nav-item {{ $isActiveSystem ? 'active' : '' }}">
                     <a
                         data-bs-toggle="collapse"
                         href="#system"
                         class="collapsed"
                         aria-expanded="false"
                     >
-                        <i class="fas fa-home"></i>
+                        <i class="fas fa-user-cog"></i>
                         <p>Quản trị hệ thống</p>
                         <span class="caret"></span>
                     </a>
-                    <div class="collapse show" id="system">
+                    <div class="collapse {{ $isActiveSystem ? 'show' : '' }}" id="system">
                         <ul class="nav nav-collapse">
-                            <li>
+                            <li class="{{ request()->routeIs([
+                                        'system.showIndexUser',
+                                        ]) ? 'active' : '' }}"
+                            >
                                 <a href="{{ route('system.showIndexUser') }}">
                                     <span class="sub-item">Danh sách người dùng</span>
                                 </a>
@@ -49,37 +74,50 @@
                         </ul>
                     </div>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{ $isActiveGeneralCatalog ? 'active' : '' }}">
                     <a
                         data-bs-toggle="collapse"
                         href="#general_catalog"
                         class="collapsed"
                         aria-expanded="false"
                     >
-                        <i class="fas fa-home"></i>
+                        <i class="fas fa-list-alt"></i>
                         <p>Danh mục chung</p>
                         <span class="caret"></span>
                     </a>
-                    <div class="collapse show" id="general_catalog">
+                    <div class="collapse {{ $isActiveGeneralCatalog ? 'show' : '' }}" id="general_catalog">
                         <ul class="nav nav-collapse">
-                            <li>
+                            <li class="{{ request()->routeIs([
+                                        'general_catalog.showIndexEmployee',
+                                        ]) ? 'active' : '' }}"
+                            >
                                 <a href="{{ route('general_catalog.showIndexEmployee') }}">
                                     <span class="sub-item">Nhân viên</span>
                                 </a>
                             </li>
-                            <li>
+                            <li class="{{ request()->routeIs([
+                                        'general_catalog.showIndexDepartment',
+                                        'general_catalog.showIndexPosition',
+                                        ]) ? 'active' : '' }}"
+                            >
                                 <a data-bs-toggle="collapse" href="#subnav1">
                                     <span class="sub-item">Phòng ban, Chức vụ</span>
                                     <span class="caret"></span>
                                 </a>
-                                <div class="collapse show" id="subnav1">
+                                <div class="collapse {{ $isActiveDepartmentPosition ? 'show' : '' }}" id="subnav1">
                                     <ul class="nav nav-collapse subnav">
-                                        <li>
+                                        <li class="{{ request()->routeIs([
+                                        'general_catalog.showIndexDepartment',
+                                        ]) ? 'active' : '' }}"
+                                        >
                                             <a href="{{ route('general_catalog.showIndexDepartment') }}">
                                                 <span class="sub-item">Phòng ban</span>
                                             </a>
                                         </li>
-                                        <li>
+                                        <li class="{{ request()->routeIs([
+                                        'general_catalog.showIndexPosition',
+                                        ]) ? 'active' : '' }}"
+                                        >
                                             <a href="{{ route('general_catalog.showIndexPosition') }}">
                                                 <span class="sub-item">Chức vụ</span>
                                             </a>
@@ -87,17 +125,26 @@
                                     </ul>
                                 </div>
                             </li>
-                            <li>
+                            <li class="{{ request()->routeIs([
+                                        'general_catalog.showIndexWorkingShift',
+                                        ]) ? 'active' : '' }}"
+                            >
                                 <a href="{{ route('general_catalog.showIndexWorkingShift') }}">
                                     <span class="sub-item">Ca làm thêm</span>
                                 </a>
                             </li>
-                            <li>
+                            <li class="{{ request()->routeIs([
+                                        'general_catalog.showIndexAllowance',
+                                        ]) ? 'active' : '' }}"
+                            >
                                 <a href="{{ route('general_catalog.showIndexAllowance') }}">
                                     <span class="sub-item">Phụ cấp và trợ cấp</span>
                                 </a>
                             </li>
-                            <li>
+                            <li class="{{ request()->routeIs([
+                                        'general_catalog.showIndexDeduction',
+                                        ]) ? 'active' : '' }}"
+                            >
                                 <a href="{{ route('general_catalog.showIndexDeduction') }}">
                                     <span class="sub-item">Trích nộp theo lương</span>
                                 </a>
@@ -122,7 +169,7 @@
                         class="collapsed"
                         aria-expanded="false"
                     >
-                        <i class="fas fa-home"></i>
+                        <i class="fas fa-clock"></i>
                         <p>Quản lý chấm công</p>
                         <span class="caret"></span>
                     </a>
@@ -143,7 +190,7 @@
                         class="collapsed"
                         aria-expanded="false"
                     >
-                        <i class="fas fa-home"></i>
+                        <i class="fas fa-calculator"></i>
                         <p>Tính lương và thanh toán</p>
                         <span class="caret"></span>
                     </a>
@@ -160,24 +207,24 @@
                 <li class="nav-item">
                     <a
                         data-bs-toggle="collapse"
-                        href="#deduction"
+                        href="#deduction_allowance"
                         class="collapsed"
                         aria-expanded="false"
                     >
-                        <i class="fas fa-home"></i>
-                        <p>Quản lý trích nộp</p>
+                        <i class="fas fa-wallet"></i>
+                        <p>Trích nộp và phụ cấp</p>
                         <span class="caret"></span>
                     </a>
-                    <div class="collapse show" id="deduction">
+                    <div class="collapse show" id="deduction_allowance">
                         <ul class="nav nav-collapse">
                             <li>
                                 <a href="#">
-                                    <span class="sub-item">Bảng tổng hợp trích lương</span>
+                                    <span class="sub-item">Bảng tổng hợp trích nộp</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#">
-                                    <span class="sub-item">Trích lương cho nhân viên</span>
+                                    <span class="sub-item">Bảng tổng hợp phụ cấp</span>
                                 </a>
                             </li>
                         </ul>
@@ -190,7 +237,7 @@
                         class="collapsed"
                         aria-expanded="false"
                     >
-                        <i class="fas fa-home"></i>
+                        <i class="fas fa-chart-line"></i>
                         <p>Hạch toán và báo cáo</p>
                         <span class="caret"></span>
                     </a>
