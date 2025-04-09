f26ac27927881677d4007dc12d129528b05bc9dd
John <sandbox.duchuy02@gmail.com>
2025-04-10 01:26:43 +0700

    fix bug: net_salary_before_tax null

--- resources/views/page/accounting/tax-table.blade.php
+++ resources/views/page/accounting/tax-table.blade.php
@@ -1,4 +1,5 @@
-@php use App\Models\Payroll; @endphp
+@php use App\Models\Employee;use App\Models\Payroll;use Illuminate\Database\Eloquent\Collection; @endphp
+@php use Carbon\Carbon; @endphp
 <table class="display table table-bordered table-hover accounting-table">
     <thead>
     <tr>
@@ -22,31 +23,45 @@
     </tr>
     </thead>
     <tbody>
-    @foreach($employees as $index => $employee)
-        @php
-            $payroll = $employee->payrolls->first();
-        @endphp
+    @php
+        /** @var Collection|Employee[] $employees */
+    @endphp
+    @php
+        $hasPayroll = $employees->filter(fn($e) => $e->payrolls->isNotEmpty())->isNotEmpty();
+    @endphp
+    @if ( $hasPayroll )
+        @foreach($employees as $index => $employee)
+            @php
+                $payroll = $employee->payrolls->first();
+            @endphp
+            <tr>
+                <td class="text-center">{{ $loop->iteration }}</td>
+                <td>{{ $employee->full_name }}</td>
+                <td>{{ $employee->tax_code }}</td>
+                <td class="text-end">{{ $payroll->net_salary_before_tax ? number_format($payroll->net_salary_before_tax) : 0 }}</td>
+                @if( empty($payroll->net_salary_before_tax) && ($payroll->net_salary_before_tax - Payroll::TAX_SELF - Payroll::TAX_DEPENDENT * $employee->number_of_dependent) < 0 )
+                    <td class="text-end">0</td>
+                    <td class="text-end">0</td>
+                    <td class="text-end">0</td>
+                    <td class="text-end">0</td>
+                    <td class="text-end">0</td>
+                @else
+                    <td class="text-end">{{ Payroll::TAX_SELF ? number_format(Payroll::TAX_SELF) : 0 }}</td>
+                    <td class="text-end">{{ $employee->number_of_dependent ?? 0 }}</td>
+                    <td class="text-end">{{ $employee->number_of_dependent ? $employee->number_of_dependent * Payroll::TAX_DEPENDENT : 0 }}</td>
+                    <td class="text-end">{{ Payroll::TAX_DEPENDENT + $employee->number_of_dependent * Payroll::TAX_DEPENDENT }}</td>
+                    <td class="text-end">{{ $payroll->net_salary_before_tax - (Payroll::TAX_DEPENDENT + $employee->number_of_dependent * Payroll::TAX_DEPENDENT) }}</td>
+                @endif
+                <td class="text-end">{{ $payroll->tax_amount ? number_format($payroll->tax_amount) : 0}}</td>
+                <td class="text-center">{{ $employee->bank_account }}</td>
+            </tr>
+        @endforeach
+    @else
         <tr>
-            <td class="text-center">{{ $index + 1 }}</td>
-            <td>{{ $employee->full_name }}</td>
-            <td>{{ $employee->tax_code }}</td>
-            <td class="text-end">{{ $payroll->net_salary_before_tax ? number_format($payroll->net_salary_before_tax) : 0 }}</td>
-            @if(($payroll->net_salary_before_tax - Payroll::TAX_SELF - Payroll::TAX_DEPENDENT * $employee->number_of_dependent) < 0 )
-                <td class="text-end">0</td>
-                <td class="text-end">0</td>
-                <td class="text-end">0</td>
-                <td class="text-end">0</td>
-                <td class="text-end">0</td>
-            @else
-                <td class="text-end">{{ Payroll::TAX_SELF ? number_format(Payroll::TAX_SELF) : 0 }}</td>
-                <td class="text-end">{{ $employee->number_of_dependent ?? 0 }}</td>
-                <td class="text-end">{{ $employee->number_of_dependent ? $employee->number_of_dependent * Payroll::TAX_DEPENDENT : 0 }}</td>
-                <td class="text-end">{{ Payroll::TAX_DEPENDENT + $employee->number_of_dependent * Payroll::TAX_DEPENDENT }}</td>
-                <td class="text-end">{{ $payroll->net_salary_before_tax - (Payroll::TAX_DEPENDENT + $employee->number_of_dependent * Payroll::TAX_DEPENDENT) }}</td>
-            @endif
-            <td class="text-end">{{ $payroll->tax_amount ? number_format($payroll->tax_amount) : 0}}</td>
-            <td class="text-center">{{ $employee->bank_account }}</td>
+            <td colspan="10" class="text-left text-muted py-3">
+                <i class="fas fa-info-circle me-1"></i> Bảng lương tháng {{ Carbon::createFromFormat('Y-m', $month)->format('m/Y') }} chưa được chốt.
+            </td>
         </tr>
-    @endforeach
+    @endif
     </tbody>
 </table>
